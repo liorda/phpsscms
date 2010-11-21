4a1abb45336bad86fd088b0221bc12a882791125
liorda@gmail.com <liorda@gmail.com@d74e9bd3-119b-fc0b-a8dc-ec3ba49c8115>
2010-11-21 14:42:47 +0000

    resolves many bugs and typos. adds features and ables the first public version.

--- trunk/funcs.php
+++ trunk/funcs.php
@@ -2,83 +2,82 @@
 
 function GeneratePage()
 {
-    $s = $_GET['s'];
-    $c = $_GET['c'];
+	$s = $_GET['s'];
+	$c = $_GET['c'];
 
-    if (!isset($s) && !isset($c)) {
+	if (!isset($s) && !isset($c)) {
 	$s = "index";
-    }
-    if (isset($s))
-	ValidifyCnS($s);
-    if (isset($c))
-	ValidifyCnS($c);
+	}
+	if (isset($s))
+		ValidifyCnS($s);
+	if (isset($c))
+		ValidifyCnS($c);
 
-    $xml = simplexml_load_file('struct.xml');
+	$xml = simplexml_load_file('struct.xml');
 
-    list($top_str, $r) = TopMenu($xml, $s);
+	list($top_str, $r) = TopMenu($xml->sections, $s);
     
-    $top_str .= "\n" . '<div id="main">' . "\n\n";
+	$top_str .= "\n" . '<div id="main">' . "\n\n";
     
-    list($side_str, $t) = SideMenu($xml, $s, $r, $c);
+	list($side_str, $t) = SideMenu($xml->sections, $s, $r, $c);
 
-    $header_str = HeaderStr($xml, $r, $t);
+	$header_str = HeaderStr($xml, $r, $t);
     
-    $content_str = ContentStr($xml, $r, $t);
+	$content_str = ContentStr($xml->sections, $r, $t);
     
-    $footer_str = FooterStr();
+	$footer_str = FooterStr($xml);
 
-    echo $header_str . $top_str . $side_str . $content_str . $footer_str;
+	echo $header_str . $top_str . $side_str . $content_str . $footer_str;
 }
 
-/* s and c should be english smal chars only! die if not: */
+/* s and c should be english small characters only! die if not: */
 function ValidifyCnS($str)
 {
   /* die if found " ", """, "'", "<", ">", "/", "\" or "." */
-  if (	preg_match("/\ /", $str) || preg_match('/\\"/', $str) || preg_match('/\</', $str) ||
-	preg_match('/\>/', $str) || preg_match('/\\\/', $str) || preg_match('/\\//', $str) ||
-	preg_match('/\\./', $str)   ) {
-	// Abort the script
-	die("Invalid request");
-  }
+	if (preg_match("/\ /", $str) || preg_match('/\\"/', $str) || preg_match('/\</', $str) ||
+		preg_match('/\>/', $str) || preg_match('/\\\/', $str) || preg_match('/\\//', $str) ||
+		preg_match('/\\./', $str)   ) {
+			// Abort the script
+			die("Invalid request");
+	}
 }
 
 function ContentStr($xml, $r, $t)
 {
-    if (isset($t) && isset($r)) {
-        $str = $xml->section[$r]->chapters->chapter[$t]->data;
+	if (isset($t) && isset($r)) {
+		$str = $xml->section[$r]->chapters->chapter[$t]->data;
 	$title = $xml->section[$r]->chapters->chapter[$t]->title;
-    }
-    elseif (isset($r)) {
-        $str = $xml->section[$r]->data;
+	}
+	elseif (isset($r)) {
+		$str = $xml->section[$r]->data;
 	$title = $xml->section[$r]->title;
-    }
-    else {
-        $str = '<br /><br /><div align="center">הדף לא קיים</div><br /><br />';
-	$title = "הדף לא קיים עדיין";
-    }
+	}
+	else {
+		$str = '<br /><br /><div align="center">הדף לא קיים</div><br /><br />';
+		$title = "הדף לא קיים עדיין";
+	}
     
-    return '<div id="text">' . "\n" . '<h1>' . $title . "</h1>\n" . $str . "</div>\n\n";
+	return '<div id="text">' . "\n" . '<h1>' . $title . "</h1>\n" . $str . "</div>\n\n";
 }
-
 
 function TopMenu($xml, $s)
 {
-    $i = -1;
-    $str = "\n\n" . '<div id="menu">' . "\n";
-    foreach ($xml->section as $section) {
-        $i++;
-        $title = $section->title;
-        $suid = $section->suid;
-        $str .= '<a';
-        if ($suid == $s) {
-            $str .= ' class="selected"';
-            $j=$i;
-        }
-        $str .= ' href="?s=' . $suid;
-        $str .= '">' . $title . "</a>&nbsp;&nbsp;&nbsp;&nbsp;";
-    }
-    $str .= "\n</div>\n";
-    return array($str,$j);
+	$i = -1;
+	$str = "\n\n" . '<div id="menu">' . "\n";
+	foreach ($xml->section as $section) {
+		$i++;
+		$title = $section->title;
+		$suid = $section->suid;
+		$str .= '<a';
+		if ($suid == $s) {
+			$str .= ' class="selected"';
+			$j=$i;
+		}
+		$str .= ' href="?s=' . $suid;
+		$str .= '">' . $title . "</a>&nbsp;&nbsp;&nbsp;&nbsp;";
+	}
+	$str .= "\n</div>\n";
+	return array($str,$j);
 }
 
 function SideMenu($xml, $s, $r, $c)
@@ -117,24 +116,26 @@ function HeaderStr($xml, $r, $t)
             "</head>\n\n" . 
             "<body>\n\n" .
             '<div id="container">' . "\n\n" .
-            '<div id="header-main"><a href="?s=index">בית הרב</a></div>' ."\n" .
-            '<div id="header-sub"><a href="?s=index">חצרות בית ה\' - קול רינה ותורה</a></div>';
+            '<div id="header-main"><a href="' . $xml->metadata->basedir . '">' . $xml->metadata->headline . '</a></div>' . "\n" .
+            '<div id="header-sub"><a href="' . $xml->metadata->basedir . '">' . $xml->metadata->subheadline . '</a></div>';
 
-    $ttl = "בית הרב - אתר הפיוטים";
+    $ttl = $xml->metadata->headline;
+	
     if (isset($r))
-        $ttl = $xml->section[$r]->title;
+        $ttl = $xml->sections->section[$r]->title;
     if (isset($t))
-        $ttl .= " - " . $xml->section[$r]->chapters->chapter[$t]->title;
+        $ttl .= " - " . $xml->sections->section[$r]->chapters->chapter[$t]->title;
+		
     $str2 = str_replace("TITLE_TOKEN", $ttl, $str);
     return $str2;
 }
 
-function FooterStr()
+function FooterStr($xml)
 {
     $str = '<div class="clear"></div>' . "\n\n".
            "</div>\n\n" .
            '<div id="footer">' . "\n" .
-           '&copy; 2010  &nbsp;<span class="separator">|</span> &nbsp; ' . "\n" . "כל הזכויות שמורות" . 
+           '&copy; ' . $xml->metadata->lastupdate->year . '  &nbsp;<span class="separator">|</span> &nbsp; ' . "\n" . "כל הזכויות שמורות ל" . $xml->metadata->copyrights . 
            "</div>\n\n</div>\n\n</body>\n</html>\n\n";
 
     return $str;
